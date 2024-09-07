e191433099291c851cfd0fcb05c13285e654ff3e
Farhan Ferdous <74263768+Farhan41@users.noreply.github.com>
2024-09-08 03:00:04 +0600

    five

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Farhan")
+function navigateToPage2() {
+    window.location.href = './pages/page2.html';
+}
+
+function navigateToPage0() {
+    window.location.href = '../index.html';
+}
+
+function navigateToPage() {
+    // window.location.href = 'index.html';
+    
+    document.getElementById('error-message').style.display = 'block';
+
+    setTimeout(function() {
+        document.getElementById('error-message').style.display = 'none';
+    }, 3000);
+    
+}
+
